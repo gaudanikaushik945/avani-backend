8efa586eab3e95f76b54f48a9d96f3500d22fabf
kaushik <gaudanikaushik945@gmail.com>
2024-12-13 17:47:50 +0530

    new debug

--- Model/mongodb.js
+++ Model/mongodb.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose")
 require("dotenv").config()
 
+mongoose.set('debug+++++++++++++++++++++++++++', true);
 
 
 mongoose.connect("mongodb+srv://kaushikgaudani945:X4YfqjM4xQtSGtKl@cluster0.b9cns.mongodb.net/gpstracker?retryWrites=true&w=majority", 
-   { useUnifiedTopology: true}
+   {}
 )
 .then(() => {
     console.log(`===== mongodb database connection successfully ======== ${"mongodb+srv://kaushikgaudani945:X4YfqjM4xQtSGtKl@cluster0.b9cns.mongodb.net/gpstracker"}` );
 }).catch((error) => {
     console.log("===== error =======", error);   
 })
-mongoose.set('debug+++++++++++++++++++++++++++', true);
 
 
 
