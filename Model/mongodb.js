04ce8c1fef74dfcf3886c51275382e708521acaa
kaushik <gaudanikaushik945@gmail.com>
2024-12-13 18:22:26 +0530

    change port

--- Model/mongodb.js
+++ Model/mongodb.js
@@ -4,8 +4,8 @@ require("dotenv").config()
 
 
 mongoose.connect("mongodb+srv://kaushikgaudani945:X4YfqjM4xQtSGtKl@cluster0.b9cns.mongodb.net/gpstracker?retryWrites=true&w=majority", 
-   { serverSelectionTimeoutMS: 30000,  // 30 seconds for server selection
-    socketTimeoutMS: 45000,  }
+   { serverSelectionTimeoutMS: 90000,  // 30 seconds for server selection
+    socketTimeoutMS: 95000,  }
 )
 .then(() => {
     console.log(`===== mongodb database connection successfully ======== ${"mongodb+srv://kaushikgaudani945:X4YfqjM4xQtSGtKl@cluster0.b9cns.mongodb.net/gpstracker"}` );
