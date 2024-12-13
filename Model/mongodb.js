931c9ae7d5eb8ec58c8102308d9e6610e0eb6920
kaushik <gaudanikaushik945@gmail.com>
2024-12-13 22:00:48 +0530

    change timeOut++++++++++--------

--- Model/mongodb.js
+++ Model/mongodb.js
@@ -4,10 +4,7 @@ require("dotenv").config()
 
 
 mongoose.connect("mongodb+srv://kaushikgaudani945:X4YfqjM4xQtSGtKl@cluster0.b9cns.mongodb.net/gpstracker?retryWrites=true&w=majority", 
-//    { serverSelectionTimeoutMS: 1000,  // 30 seconds for server selection
-//     socketTimeoutMS: 1000,  }
-{useNewUrlParser: true,
-useUnifiedTopology: true,}
+{ serverSelectionTimeoutMS: 30000 }
 )
 .then(() => {
     console.log(`===== mongodb database connection successfully ======== ${"mongodb+srv://kaushikgaudani945:X4YfqjM4xQtSGtKl@cluster0.b9cns.mongodb.net/gpstracker"}` );
