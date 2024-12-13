b783a7477e851271239532231031006f07a9a66c
kaushik <gaudanikaushik945@gmail.com>
2024-12-13 17:39:12 +0530

    new url------------------>==================

--- Model/mongodb.js
+++ Model/mongodb.js
@@ -3,12 +3,14 @@ require("dotenv").config()
 
 
 
-mongoose.connect("mongodb+srv://kaushikgaudani945:X4YfqjM4xQtSGtKl@cluster0.b9cns.mongodb.net/gpstracker?retryWrites=true&w=majority", {
-}).then(() => {
+mongoose.connect("mongodb+srv://kaushikgaudani945:X4YfqjM4xQtSGtKl@cluster0.b9cns.mongodb.net/gpstracker?retryWrites=true&w=majority", 
+ {useNewUrlParser: true, useUnifiedTopology: true}  )
+.then(() => {
     console.log(`===== mongodb database connection successfully ======== ${"mongodb+srv://kaushikgaudani945:X4YfqjM4xQtSGtKl@cluster0.b9cns.mongodb.net/gpstracker"}` );
 }).catch((error) => {
     console.log("===== error =======", error);   
 })
+mongoose.set('debug+++++++++++++++++++++++++++', true);
 
 
 
