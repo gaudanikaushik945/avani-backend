12057c2d6c365d5272ec7ca0012ed62009207086
kaushik <gaudanikaushik945@gmail.com>
2024-12-14 11:20:46 +0530

    change socket

--- Model/mongodb.js
+++ Model/mongodb.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 
 
 
-mongoose.connect("mongodb+srv://kaushikgaudani945:X4YfqjM4xQtSGtKl@cluster0.b9cns.mongodb.net/gpstracker?retryWrites=true&w=majority", 
+mongoose.connect("mongodb+srv://kaushikgaudani945:X4YfqjM4xQtSGtKl@cluster0.b9cns.mongodb.net/gpstracker", 
 )
 .then(() => {
     console.log(`===== mongodb database connection successfully ======== ${"mongodb+srv://kaushikgaudani945:X4YfqjM4xQtSGtKl@cluster0.b9cns.mongodb.net/gpstracker"}` );
